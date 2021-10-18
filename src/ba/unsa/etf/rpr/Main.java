47e38a37b081a323842d3df05f17d09c40462a1a
Merim <mkulovac1@etf.unsa.ba>
2021-10-18 17:34:14 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int sumaCifara(int n) {
+        int suma = 0;
+        while(n != 0) {
+            suma += (n % 10);
+            n /= 10;
+        }
+
+        return suma;
+    }
+
+
+    public static void main(String[] args) {
+
+            System.out.println("Unesite broj n: ");
+            Scanner ulaz = new Scanner(System.in);
+            int n = ulaz.nextInt();
+            for(int i = 1; i <= n; i++) {
+                if(i % sumaCifara(i) == 0)
+                    System.out.print(i + " ");
+            }
+    }
+}
